ba420add2a0cb830cac73f021275964e8eed7cd7
Jion Choi <chlwldhs@gmail.com>
2024-02-15 21:46:20 -0700

    js update

--- mainPage/index.js
+++ mainPage/index.js
@@ -15,8 +15,8 @@ app.listen(8080, function(){
 });
 
 //if the user visits /game website, run the code inside the function. This "/game" can be modified 
-app.get('/gam', function(req, res){
-    res.send("This is main page of the card game");
+app.get('/logIn', function(req, res){
+    res.send("This is log In page");
 });
 
 // "/" one slash is for home page
