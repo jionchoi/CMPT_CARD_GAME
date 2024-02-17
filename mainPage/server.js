b74f72317913b122b197b1301932a164e31d8a58
Jion Choi <chlwldhs@gmail.com>
2024-02-16 21:12:26 -0700

    node.js server commit

---
+++ mainPage/server.js
@@ -0,0 +1,57 @@
+//open the server (basic setting for opening the server)
+const express = require("express");
+const app = express();
+//cors error is when other html files access to the server or request anything from the server, it cors blocks them. 
+var cors = require('cors');
+
+//we can chage the condition of cors. (deny access from certain ways)
+app.use(cors());
+
+//8080 is the port number of the server and run the code inside the function
+//localhost:8080
+app.listen(8080, function(){
+    console.log("listening on 8080");
+});
+
+//if the user visits /game website, run the code inside the function. This "/game" can be modified 
+app.get('/logIn', function(req, res){
+    res.send("This is log In page");
+});
+
+// "/" one slash is for home page
+app.get('/', function(req, res){
+    res.sendFile(__dirname + "/logIn/logIn.html");
+});
+
+// ?q=jion&name=jion&age=18 => {q = "jion", name = "jion", age = 18}
+app.get('/user/:userId', function(req, res){
+    const {userId} = req.params;
+    if(userId == "jion"){
+        res.json({"username": userId});
+    }
+    else{
+        res.json({"username": "Access Denied"});
+    }
+});
+
+
+// //creating database
+// var MongoClient = require('mongodb').MongoClient;
+// var url = "mongodb://localhost:8080/mydb";
+
+// MongoClient.connect(url, function(err, db) {
+//     if (err) throw err;
+//     console.log("Database created!");
+//     db.close();
+//   });
+
+function logIn(){
+    const username = document.getElementById("username").value;
+    
+    fetch(`http://localhost:8080/user/${username}`)
+    .then((response) => response.json())
+    .then((data) =>{
+        document.getElementById("main").innerHTML = data;
+    });
+   }
+
