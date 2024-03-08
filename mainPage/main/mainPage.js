a94fe17f685f1cc39be78c4959415996b20369a0
CamoPaw252 <93169276+CamoPaw252@users.noreply.github.com>
2024-03-08 09:58:32 -0700

    Fix Conflicts

--- mainPage/main/mainPage.js
+++ mainPage/main/mainPage.js
@@ -1,1 +1,40 @@
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
 
+
+  //A general function to open a sidebar, the parameter is an id name
+  function openNav(idName){
+    element = document.getElementById(idName).style; 
+    element.transition = "0.5s"; 
+    element.width = "250px"; 
+    element.padding = "20px"
+
+
+
+  }
+
+   //A general function to close a sidebar, the parameter is an id name
+  function closeNav(idName){
+    element = document.getElementById(idName).style; 
+    element.width = "0";
+    element.transition = "0s";
+    element.padding = "0px";
+    element.transition = "0.5s"
+  }
